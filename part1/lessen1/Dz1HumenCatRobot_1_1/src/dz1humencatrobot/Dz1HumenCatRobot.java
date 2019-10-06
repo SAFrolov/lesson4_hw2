bd1902c9502fdd55df2b115e140deb139ff878b2
safrolov <safrolov@192.168.1.37>
2019-10-07 00:01:50 +0300

    The first version

--- part1/lessen1/Dz1HumenCatRobot_1_1/src/dz1humencatrobot/Dz1HumenCatRobot.java
+++ part1/lessen1/Dz1HumenCatRobot_1_1/src/dz1humencatrobot/Dz1HumenCatRobot.java
@@ -6,14 +6,6 @@
 
 package dz1humencatrobot;
 
-import pri.Wall;
-import pri.RunPath;
-import pri.Pripatstvie;
-import dz1humencatrobot.sth.Robot;
-import dz1humencatrobot.sth.Humen;
-import dz1humencatrobot.sth.Sthing;
-import dz1humencatrobot.sth.Cat;
-
 /**
  *
  * @author safrolov
@@ -24,28 +16,7 @@ public class Dz1HumenCatRobot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Humen h = new Humen();
-        Cat c = new Cat();
-        Robot r = new Robot();
-        
-        Pripatstvie [] myPrip = new Pripatstvie[] {new Wall(1), 
-            new RunPath(50), new Wall(2), new RunPath(100),new RunPath(3),new Wall(2),new RunPath(150)};
-        Sthing [] mySt = new Sthing []{new Humen(), new Cat(), new Humen(), new Robot()};
-        
-        for (int i = 0; i < mySt.length; i++) {
-            System.out.println(mySt[i].getInfo());
-            for (int j = 0; j < myPrip.length; j++) {
-                System.out.println(myPrip[j].getInfo());
-                if (myPrip[j].doit(mySt[i])==false){
-                    System.out.println("finished");
-                    System.out.println();
-                    break;
-                }
-                    
-                    
-            }
- 
-        }
+
     }
     
 }
